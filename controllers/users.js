ee3cbf285e608bb73710ef4b64eb05e735d7d682
Alvinator24 <alvinalvin3065@gmail.com>
2024-04-01 14:09:15 +0800

    uploads folder removed, no longer needed

--- controllers/users.js
+++ controllers/users.js
@@ -13,9 +13,6 @@ const Comment = require('../models/comment')
 
 // image upload
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads')
-    },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
     }
